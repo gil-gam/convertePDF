17e8a26cfc37afe1a81eb372f60ab80badd477e9
Gil Gam <gilbertoandreattamaia@gmail.com>
2022-08-29 00:58:34 -0300

    comitando em 29/08/2022

---
+++ Converte_PDF_Texto/ConvertePDF.cs
@@ -0,0 +1,51 @@
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.IO;
+using System.Text;
+
+namespace Converte_PDF_Texto
+{
+    public class ConvertePDF
+    {
+
+        public string ExtrairTexto_PDF(string caminho)
+        {
+
+            //if (File.Exists(caminho))
+            //{
+            using (PdfReader leitor = new PdfReader(caminho))
+            {
+                StringBuilder texto = new StringBuilder();
+
+                // no pdf com 4 colunas, captura da quarta para a primeira coluna (ou seja, da direita p/ esquerda);
+                for (int page = 1; page <= leitor.NumberOfPages; page++)
+                {
+                    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                    string currentText = PdfTextExtractor.GetTextFromPage(leitor, page, strategy);
+
+                    currentText = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(currentText)));
+                    texto.Append(currentText);
+                }
+                //texto.Close();
+                return texto.ToString();
+            }
+            //}
+
+        }
+
+
+        //public string ExtrairTexto_PDF(string caminho)
+        //{
+        //    using (PdfReader leitor = new PdfReader(caminho))
+        //    {
+        //        StringBuilder texto = new StringBuilder();
+
+        //        for (int i = 1; i <= leitor.NumberOfPages; i++)
+        //        {
+        //            texto.Append(PdfTextExtractor.GetTextFromPage(leitor, i));
+        //        }
+        //        return texto.ToString();
+        //    }
+        //}
+    }
+}
---
+++ Converte_PDF_Texto/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Converte_PDF_Texto
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvertePDF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ConvertePDF pdftxt = new ConvertePDF();
+                txtArquivoTexto.Text = pdftxt.ExtrairTexto_PDF(txtCaminoNomePDF.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnProcurar_Click(object sender, EventArgs e)
+        {
+            //define as propriedades do controle 
+            //OpenFileDialog
+            this.ofd1.Multiselect = false;
+            this.ofd1.Title = "Selecionar PDF";
+            ofd1.InitialDirectory = @"C:\dados";
+            //filtra para exibir somente arquivos de imagens
+            ofd1.Filter = "Files (*.PDF)|*.PDF|" + "All files (*.*)|*.*";
+            ofd1.CheckFileExists = true;
+            ofd1.CheckPathExists = true;
+            ofd1.FilterIndex = 2;
+            ofd1.RestoreDirectory = true;
+            ofd1.ReadOnlyChecked = true;
+            ofd1.ShowReadOnly = false;
+            
+            DialogResult dr = this.ofd1.ShowDialog();
+
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                txtCaminoNomePDF.Text = ofd1.FileName;
+            }
+        }
+
+        private void txtCaminoNomePDF_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
